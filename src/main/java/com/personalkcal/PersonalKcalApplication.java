65593ab5110c6e7011cdafb6e0ceb1baa41fccd6
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-06 18:25:17 +0900

    ..

--- src/main/java/com/personalkcal/PersonalKcalApplication.java
+++ src/main/java/com/personalkcal/PersonalKcalApplication.java
@@ -1,5 +1,7 @@
 package com.personalkcal;
 
+import com.personalkcal.domain.Member;
+import com.personalkcal.service.KcalService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonalKcalApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PersonalKcalApplication.class, args);
+
+
 	}
 
 }

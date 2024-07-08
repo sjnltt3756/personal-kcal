4a884eb391b9afcc99c2809ecab61d6422a1260c
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-08 23:53:15 +0900

    빌더 패턴 적용

--- src/main/java/com/personalkcal/domain/Member.java
+++ src/main/java/com/personalkcal/domain/Member.java
@@ -1,31 +1,61 @@
 package com.personalkcal.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 
-@Getter @Setter
+@Getter
+@Entity
+@Builder
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+    private String gender;
 
     private int height;
     private int weight;
     private int age;
-    private String gender;
 
-    private Member(int height, int weight, int age, String gender) {
+    // private 생성자
+
+    private Member(int height, int weight, int age, String gender, String nickname) {
         this.height = height;
         this.weight = weight;
         this.age = age;
         this.gender = gender;
+        this.nickname = nickname;
     }
 
-    public static Member specOfWoman(int height, int weight, int age, String gender){
-        return new Member(height, weight, age, gender);
-    }
-    public static Member specOfMan(int height, int weight, int age, String gender){
-        return new Member(height, weight, age, gender);
+    /**
+     * 정적 팩토리 메서드
+     */
+    public static Member specOfWoman(int height, int weight, int age, String nickname) {
+        return Member.builder()
+                .height(height)
+                .weight(weight)
+                .age(age)
+                .gender("여성")
+                .nickname(nickname)
+                .build();
     }
 
+    public static Member specOfMan(int height, int weight, int age, String nickname) {
+        return Member.builder()
+                .height(height)
+                .weight(weight)
+                .age(age)
+                .gender("남성")
+                .nickname(nickname)
+                .build();
+    }
 
 
 }

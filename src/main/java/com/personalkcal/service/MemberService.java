3107e3db16cb98980fc4594cade72cc8a55a4ff5
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-09 00:55:00 +0900

    회원 정보 출력 서비스

--- src/main/java/com/personalkcal/service/MemberService.java
+++ src/main/java/com/personalkcal/service/MemberService.java
@@ -1,11 +1,14 @@
 package com.personalkcal.service;
 
 import com.personalkcal.Dto.LoginDTO;
+import com.personalkcal.Dto.MemberDTO;
 import com.personalkcal.mapper.MemberMapper;
 import com.personalkcal.domain.Member;
 import com.personalkcal.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +29,8 @@ public class MemberService {
                 .orElseThrow(() -> new RuntimeException("회원을 찾을 수 없습니다."));
         return memberMapper.toLoginDto(member);
     }
+
+
 
     /**
      * 여성 닉네임/스펙 입력
@@ -49,5 +54,15 @@ public class MemberService {
     public Member createMan(int height, int weight,int age,String nickname){
         Member man = Member.specOfMan(height, weight, age,nickname);
         return memberRepository.save(man);
+    }
+
+    /**
+     * 회원 정보 출력
+     * @param mNo
+     * @return
+     */
+    public MemberDTO viewMember(Long mNo){
+        Member member = memberRepository.findById(mNo).orElseThrow(() -> new RuntimeException("해당 회원을 찾을 수 없습니다."));
+        return memberMapper.toMemberDTO(member);
     }
 }

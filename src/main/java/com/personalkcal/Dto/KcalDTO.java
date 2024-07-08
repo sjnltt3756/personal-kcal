af19660c9452c142b1be40663f943f7780f18ada
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-09 00:30:42 +0900

    ..

--- src/main/java/com/personalkcal/Dto/KcalDTO.java
+++
@@ -1,9 +0,0 @@
-package com.personalkcal.Dto;
-
-public class KcalDTO {
-
-    private double dietKcal;
-    private double maintainKcal;
-    private double massUpKcal;
-    private double bulkUpKcal;
-}
--- src/main/java/com/personalkcal/service/KcalService.java
+++ src/main/java/com/personalkcal/service/KcalService.java
@@ -2,26 +2,41 @@ package com.personalkcal.service;
 
 
 import com.personalkcal.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+
 @Service
+@RequiredArgsConstructor
 public class KcalService {
+    private final Member member;
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+
+
 
     // 체형별 탄단지 비율 계산
     // 다이어트
-    public static double dietKcal(Member member){
-       return basicKcal(member)-500;
+    public double dietKcal(Member member){
+        if (member.getGender().equals("남성")){
+
+            return basicKcal(member)-500;
+        }else{
+            return basicKcal(member)-300;
+        }
     }
     // 유지
-    public static double maintainKcal(Member member){
+    public double maintainKcal(Member member){
         return basicKcal(member);
     }
     // 린매스업
-    public static double massUpKcal(Member member){
+    public double massUpKcal(Member member){
         return basicKcal(member)+200;
     }
     // 벌크업
-    public static double bulkUpKcal(Member member){
+    public double bulkUpKcal(Member member){
         return basicKcal(member)+500;
     }
 
@@ -31,15 +46,18 @@ public class KcalService {
 
 
     // 기본 권장 칼로리 계산
-    private static Double basicKcal(Member member){
+    private Double basicKcal(Member member){
         double womanKcal = 447.593 + (9.247 * member.getWeight()) + (3.098 * member.getHeight()) - (4.330 * member.getAge());
         double manKcal = 88.362 + (13.397 * member.getWeight()) + (4.799 * member.getHeight()) - (6.677 * member.getAge());
 
-        if (member.getGender().equals("여성")) {
-            return womanKcal;
-        }else{
-            return manKcal;
+        double kcal;
+        if ("여성".equals(member.getGender())) {
+            kcal = womanKcal;
+        } else {
+            kcal = manKcal;
         }
+
+        return Double.parseDouble(df.format(kcal));
 
     }
 }

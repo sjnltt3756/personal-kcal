e50373d90d146ca05110ed3a669d1cde0910ddca
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-09 00:28:28 +0900

    Dto Entity 매핑 라이브러리 map struct 사용

---
+++ src/main/java/com/personalkcal/mapper/MemberMapper.java
@@ -0,0 +1,27 @@
+package com.personalkcal.mapper;
+
+import com.personalkcal.Dto.LoginDTO;
+import com.personalkcal.Dto.MemberDTO;
+import com.personalkcal.domain.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MemberMapper {
+
+    MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
+
+    @Mapping(target = "id", ignore = true) // ID는 자동 생성되므로 매핑에서 제외
+    MemberDTO toMemberDTO(Member member);
+
+    LoginDTO toLoginDto(Member member);
+
+
+   /*
+   @Mapping(target = "id", ignore = true) // ID는 자동 생성되므로 매핑에서 제외
+    Member toMember(MemberDTO memberDTO);
+    @Mapping(source = "nickname",target = "nickname")
+    Member toLogin(LoginDTO LoginDTO);
+    */
+}

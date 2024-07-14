4c5e07d63e7bdd23a7b4f6adf86f9586c92d5f1c
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-15 01:02:20 +0900

    mapper struct설정

--- src/main/java/com/personalkcal/mapper/MemberMapper.java
+++ src/main/java/com/personalkcal/mapper/MemberMapper.java
@@ -4,8 +4,10 @@ import com.personalkcal.Dto.LoginDTO;
 import com.personalkcal.Dto.MemberDTO;
 import com.personalkcal.domain.Member;
 import org.mapstruct.Mapper;
+
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
@@ -14,16 +16,15 @@ public interface MemberMapper {
 
     MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
 
+
     @Mapping(target = "id", ignore = true) // ID는 자동 생성되므로 매핑에서 제외
+    Member toMember(MemberDTO memberDto);
+
     MemberDTO toMemberDTO(Member member);
 
+    @Mapping(source = "nickname", target = "nickname")
     LoginDTO toLoginDto(Member member);
 
 
-   /*
-   @Mapping(target = "id", ignore = true) // ID는 자동 생성되므로 매핑에서 제외
-    Member toMember(MemberDTO memberDTO);
-    @Mapping(source = "nickname",target = "nickname")
-    Member toLogin(LoginDTO LoginDTO);
-    */
+
 }

d21e6d79f3e01ca73405f9e2c16875b3e97b691b
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-15 20:00:41 +0900

    리퀘스트 매핑 제거

--- src/main/java/com/personalkcal/controller/MemberController.java
+++ src/main/java/com/personalkcal/controller/MemberController.java
@@ -6,15 +6,14 @@ import com.personalkcal.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
+
 
     @PostMapping("/login")
     public ResponseEntity<LoginDTO> loginMember(@RequestBody LoginDTO loginDTO) {

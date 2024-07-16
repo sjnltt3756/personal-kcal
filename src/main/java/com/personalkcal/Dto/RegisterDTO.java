8ee3a262d990da73dfa7591f816bfd9133e09851
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-16 18:18:27 +0900

    ..

---
+++ src/main/java/com/personalkcal/Dto/RegisterDTO.java
@@ -0,0 +1,14 @@
+package com.personalkcal.Dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterDTO {
+
+    private String nickname;
+    private String gender;
+    private int height;
+    private int weight;
+    private int age;
+
+}

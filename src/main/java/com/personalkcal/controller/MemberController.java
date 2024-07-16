36083837f63ce737a466b4b51bb5c5ac6a13fc28
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-16 18:17:46 +0900

    viewMember 수정

--- src/main/java/com/personalkcal/controller/MemberController.java
+++ src/main/java/com/personalkcal/controller/MemberController.java
@@ -2,6 +2,10 @@ package com.personalkcal.controller;
 
 import com.personalkcal.Dto.LoginDTO;
 import com.personalkcal.Dto.MemberDTO;
+import com.personalkcal.Dto.RegisterDTO;
+import com.personalkcal.domain.Member;
+import com.personalkcal.mapper.MemberMapper;
+import com.personalkcal.service.KcalService;
 import com.personalkcal.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,10 +14,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
-
+    private final KcalService kcalService;
+    private final MemberMapper memberMapper;
 
     @PostMapping("/login")
     public ResponseEntity<LoginDTO> loginMember(@RequestBody LoginDTO loginDTO) {
@@ -22,15 +28,23 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<MemberDTO> registerMember(@RequestBody MemberDTO memberDTO) {
-        MemberDTO registeredMember = memberService.registerMember(memberDTO);
+    public ResponseEntity<RegisterDTO> registerMember(@RequestBody RegisterDTO registerDTO) {
+        RegisterDTO registeredMember = memberService.registerMember(registerDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredMember);
     }
 
 
-    @GetMapping("/{mNo}")
-    public ResponseEntity<MemberDTO> viewMember(@PathVariable Long mNo) {
-        MemberDTO memberDTO = memberService.viewMember(mNo);
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberDTO> viewMember(@PathVariable Long id) {
+        MemberDTO memberDTO = memberService.viewMember(id);
+        // MemberDTO를 Member 엔티티로 매핑
+        Member member = memberMapper.toMember(memberDTO);
+
+        // 회원의 칼로리 정보를 계산하여 설정합니다.
+        kcalService.calculateKcalForMember(member);
+
+        // Member 엔티티를 다시 MemberDTO로 매핑하여 반환합니다.
+        memberDTO = memberMapper.toMemberDTO(member);
         return ResponseEntity.ok(memberDTO);
     }
 

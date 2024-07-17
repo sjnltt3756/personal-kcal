7fd769ee77df039f50103317f7d1e6e01b8e16c6
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-17 14:18:00 +0900

    ..

--- src/main/java/com/personalkcal/controller/MainController.java
+++ src/main/java/com/personalkcal/controller/MainController.java
@@ -1,13 +1,17 @@
 package com.personalkcal.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class MainController {
 
-    @GetMapping("/")
-    public String main(){
-        return "index";
+    @RequestMapping("/index")
+    public ModelAndView main(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
 }

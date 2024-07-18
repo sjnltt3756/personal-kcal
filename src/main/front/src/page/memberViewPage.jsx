a825473b4ccec2548aac362c3714da1d0e612ffe
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-18 13:26:21 +0900

    frontend react

---
+++ src/main/front/src/page/memberViewPage.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { useParams } from "react-router-dom";
+// import ViewMember from "../components/viewMember";
+
+// const MemberViewPage = () => {
+//   const { id } = useParams();
+
+//   return (
+//     <div>
+//       <h1>Member Information</h1>
+//       {id && <ViewMember memberId={id} />}
+//     </div>
+//   );
+// };
+
+// export default MemberViewPage;

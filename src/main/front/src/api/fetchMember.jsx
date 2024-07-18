82f1ccec0aae6db45117034d9ff335c67703caa5
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-18 13:26:34 +0900

    api 연동

---
+++ src/main/front/src/api/fetchMember.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080";
+
+export const loginMember = async (loginDTO) => {
+  try {
+    const response = await axios.post(`${API_URL}/member/login`, loginDTO);
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in:", error);
+    throw error;
+  }
+};
+
+export const registerMember = async (registerDTO) => {
+  try {
+    const response = await axios.post(`${API_URL}/member/register`, registerDTO);
+    return response.data;
+  } catch (error) {
+    console.error("Error registering:", error);
+    throw error;
+  }
+};
+
+export const viewMember = async (id) => {
+  try {
+    const response = await axios.get(`${API_URL}/member/view/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching member:", error);
+    throw error;
+  }
+};

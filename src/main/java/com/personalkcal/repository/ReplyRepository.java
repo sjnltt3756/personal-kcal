809e7a08578b76827f9d28ec18efe78a87801738
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-26 14:12:51 +0900

    board,reply repository

---
+++ src/main/java/com/personalkcal/repository/ReplyRepository.java
@@ -0,0 +1,8 @@
+package com.personalkcal.repository;
+
+import com.personalkcal.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply,Long> {
+
+}

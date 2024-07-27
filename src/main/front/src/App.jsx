b58df840daa33dac7214c9dee5af87b05f961a95
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-27 21:34:38 +0900

    게시판 개발

--- src/main/front/src/App.jsx
+++ src/main/front/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/login";
-import Register from "./components/register";
-import ViewMember from "./components/viewMember";
+import Login from "./components/members/login";
+import Register from "./components/members/register";
+import ViewMember from "./components/members/viewMember";
 import { Main } from "./components/main";
+import BoardComponent from "./components/boards/board";
 const App = () => {
   return (
     <div>
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='member/login' element={<Login />} />
         <Route path='member/register' element={<Register />} />
         <Route path='member/view/:id' element={<ViewMember />} />
+        <Route path='board/list' element={<BoardComponent />} />
       </Routes>
     </div>
   );
--- src/main/front/src/components/main.jsx
+++ src/main/front/src/components/main.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 export function Main({ loggedInMemberId }) {
   return (
     <div>
-      <h1>Main Page</h1>
+      <h1>칼로리 계산해드려요~</h1>
       <ul>
         <li>
-          <Link to='/member/register'>Register</Link>
+          <Link to='/member/register'>회원가입</Link>
         </li>
         <li>
-          <Link to='/member/login'>Login</Link>
+          <Link to='/member/login'>로그인</Link>
+        </li>
+        <li>
+          <Link to='/board/list'>게시판</Link>
         </li>
       </ul>
     </div>

8c4500fe29ff5306a35c634fb9588bc898f2ee74
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-28 12:14:38 +0900

    프론트 게시판 개발중

--- src/main/front/src/App.jsx
+++ src/main/front/src/App.jsx
@@ -5,7 +5,10 @@ import Login from "./components/members/login";
 import Register from "./components/members/register";
 import ViewMember from "./components/members/viewMember";
 import { Main } from "./components/main";
-import BoardComponent from "./components/boards/board";
+import BoardWrite from "./components/boards/boardWrite";
+import BoardUpdate from "./components/boards/boardUpdate";
+import BoardList from "./components/boards/boardList";
+import BoardView from "./components/boards/boardView";
 const App = () => {
   return (
     <div>
@@ -14,7 +17,10 @@ const App = () => {
         <Route path='member/login' element={<Login />} />
         <Route path='member/register' element={<Register />} />
         <Route path='member/view/:id' element={<ViewMember />} />
-        <Route path='board/list' element={<BoardComponent />} />
+        <Route path='board/list' element={<BoardList />} />
+        <Route path='board/write' element={<BoardWrite />} />
+        <Route path='board/update:id' element={<BoardUpdate />} />
+        <Route path='board/view/:id' element={<BoardView />} />
       </Routes>
     </div>
   );

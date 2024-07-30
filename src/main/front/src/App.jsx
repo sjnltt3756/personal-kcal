f012311d6c8d40e7d6a7c29b26346e7e69ba3e00
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-30 20:33:01 +0900

    리코일로 전역상태 관리

--- src/main/front/src/App.jsx
+++ src/main/front/src/App.jsx
@@ -9,19 +9,22 @@ import BoardWrite from "./components/boards/boardWrite";
 import BoardUpdate from "./components/boards/boardUpdate";
 import BoardList from "./components/boards/boardList";
 import BoardView from "./components/boards/boardView";
+import { RecoilRoot } from "recoil";
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route index element={<Main />} />
-        <Route path='member/login' element={<Login />} />
-        <Route path='member/register' element={<Register />} />
-        <Route path='member/view/:id' element={<ViewMember />} />
-        <Route path='board/list' element={<BoardList />} />
-        <Route path='board/write' element={<BoardWrite />} />
-        <Route path='board/update:id' element={<BoardUpdate />} />
-        <Route path='board/view/:id' element={<BoardView />} />
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path='member/login' element={<Login />} />
+          <Route path='member/register' element={<Register />} />
+          <Route path='member/view/:id' element={<ViewMember />} />
+          <Route path='board/list' element={<BoardList />} />
+          <Route path='board/write' element={<BoardWrite />} />
+          <Route path='board/update:boardId' element={<BoardUpdate />} />
+          <Route path='board/view/:boardId' element={<BoardView />} />
+        </Routes>
+      </RecoilRoot>
     </div>
   );
 };

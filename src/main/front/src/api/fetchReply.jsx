21c62a2653c8f8c6f2ddbd54032e304610478fa4
sjnltt3756 <sjnltt3756@gmail.com>
2024-07-30 20:33:13 +0900

    게시판 / 댓글 구현

--- src/main/front/src/api/fetchReply.jsx
+++ src/main/front/src/api/fetchReply.jsx
@@ -1,31 +1,31 @@
 import axiosInstance from "./axiosInstance";
 
 export const writeReply = async (replyDTO) => {
-    try {
-      const response = await axiosInstance.post("/reply/write", replyDTO);
-      return response.data;
-    } catch (error) {
-      console.error("Error writing reply:", error);
-      throw error;
-    }
-  };
-  
-  export const updateReply = async (id, updateDTO) => {
-    try {
-      const response = await axiosInstance.put(`/reply/update/${id}`, updateDTO);
-      return response.data;
-    } catch (error) {
-      console.error("Error updating reply:", error);
-      throw error;
-    }
-  };
-  
-  export const deleteReply = async (id) => {
-    try {
-      const response = await axiosInstance.delete(`/reply/delete/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error("Error deleting reply:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await axiosInstance.post("/reply/write", replyDTO);
+    return response.data;
+  } catch (error) {
+    console.error("Error writing reply:", error);
+    throw error;
+  }
+};
+
+export const updateReply = async (id, updateDTO) => {
+  try {
+    const response = await axiosInstance.put(`/reply/update/${id}`, updateDTO);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating reply:", error);
+    throw error;
+  }
+};
+
+export const deleteReply = async (id) => {
+  try {
+    const response = await axiosInstance.delete(`/reply/delete/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting reply:", error);
+    throw error;
+  }
+};

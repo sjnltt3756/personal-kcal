d38de55f815966f7831c490660112fdbda99c3d5
sjnltt3756 <sjnltt3756@gmail.com>
2024-09-18 22:59:47 +0900

    주석 해제

--- src/test/java/com/personalkcal/PersonalKcalApplicationTests.java
+++ src/test/java/com/personalkcal/PersonalKcalApplicationTests.java
@@ -1,13 +1,14 @@
-//package com.personalkcal;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class PersonalKcalApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.personalkcal;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class PersonalKcalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

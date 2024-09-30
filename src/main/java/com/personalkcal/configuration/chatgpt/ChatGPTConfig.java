dea2500f60754d80ee106f38e82c81f168a26983
sjnltt3756 <sjnltt3756@gmail.com>
2024-09-30 16:52:57 +0900

    chatGPT 주석처리

--- src/main/java/com/personalkcal/configuration/chatgpt/ChatGPTConfig.java
+++ src/main/java/com/personalkcal/configuration/chatgpt/ChatGPTConfig.java
@@ -1,35 +1,35 @@
-package com.personalkcal.configuration.chatgpt;
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-public class ChatGPTConfig {
-
-    @Value("${openai.api.key}")
-    private String secretKey;
-
-    @PostConstruct
-    public void init() {
-        System.out.println("OpenAI API Key: " + secretKey);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate;
-    }
-
-    @Bean
-    public HttpHeaders httpHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + secretKey);
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return headers;
-    }
-}
+//package com.personalkcal.configuration.chatgpt;
+//
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Configuration
+//public class ChatGPTConfig {
+//
+//    @Value("${openai.api.key}")
+//    private String secretKey;
+//
+//    @PostConstruct
+//    public void init() {
+//        System.out.println("OpenAI API Key: " + secretKey);
+//    }
+//
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        return restTemplate;
+//    }
+//
+//    @Bean
+//    public HttpHeaders httpHeaders() {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Authorization", "Bearer " + secretKey);
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        return headers;
+//    }
+//}
